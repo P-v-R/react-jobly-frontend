fe6c33ad0a531d8b86bfe3942a733f6609d3ef8e
Perryvon <Perry.j.vr@gmail.com>
2021-06-01 12:20:51 -0700

    added some boilerplate tro routes and exported everything

--- src/Routes.js
+++ src/Routes.js
@@ -1,2 +1,12 @@
 import React from "react";
 import {Routes, Redirect, Switch} from "react-router-dom";
+import Homepage from "./Homepage";
+import CompanyList from "./CompanyList";
+import CompanyDetail from "./CompanyDetail";
+import JobList from "./JobList";
+import LoginForm from "./LoginForm";
+import SignupForm from "./SignupForm";
+import ProfileForm from "./ProfileForm";
+
+
+export default Routes;
